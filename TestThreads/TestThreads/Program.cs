8a81585939838da3769865c71500e9bcddb996ba
LAPTOP-9KQD6BO4\s_moe <s.moeen.kamali@gmail.com>
2022-11-13 23:51:52 +0330

    working on threads

---
+++ TestThreads/TestThreads/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine( Thread.CurrentThread.ManagedThreadId);
+var res = CheckThreads();
+Console.ReadLine();
+
+
+async Task<int> CheckThreads()
+{
+    int x = 1;
+    Console.WriteLine(AppDomain.GetCurrentThreadId());
+
+    await Task.Run(() => Console.WriteLine("ddd" + Thread.CurrentThread.ManagedThreadId));
+  await  Task.Delay(100);
+    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+    return 1;
+}
